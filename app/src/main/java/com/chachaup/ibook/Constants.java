6b9178a740334149d8023cfec9561b59296460a6
chacha <chachaerickjo@gmail.com>
2022-09-02 15:20:18 +0300

    ft - add base url and API key management

--- app/src/main/java/com/chachaup/ibook/Constants.java
+++ app/src/main/java/com/chachaup/ibook/Constants.java
@@ -1,5 +1,7 @@
 package com.chachaup.ibook;
 
+
 public class Constants {
     public static final String BASE_URL = "https://api.nytimes.com/svc/books/v3/lists/";
+    public static final String API_KEY = BuildConfig.API_KEY;
 }
--- app/src/main/java/com/chachaup/ibook/network/BookClient.java
+++ app/src/main/java/com/chachaup/ibook/network/BookClient.java
@@ -1,7 +1,14 @@
 package com.chachaup.ibook.network;
 
+import static com.chachaup.ibook.Constants.API_KEY;
 import static com.chachaup.ibook.Constants.BASE_URL;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,8 +16,21 @@ public class BookClient {
     public static Retrofit retrofit = null;
     public static BookAPI getClient(){
         if (retrofit == null){
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .addHeader("Authorization",API_KEY)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
